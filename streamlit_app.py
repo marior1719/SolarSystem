eb7fe33c48640267cc40efdb60d61a06e17b3a44
marior1719 <marior1719@gmail.com>
2026-03-01 00:47:13 +0000

    Push new

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new Streamlit app Mario")
+st.title("🎈 My new Streamlit app Mario Moreno prueba 2")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
